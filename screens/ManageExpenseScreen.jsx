ea8a7fc775a495062ea1cb31c84daf65433e055b
Jason Torres Luis <jtorresl79c@gmail.com>
2023-08-22 20:37:39 -0700

    163. Sending POST Http Requests

--- screens/ManageExpenseScreen.jsx
+++ screens/ManageExpenseScreen.jsx
@@ -5,6 +5,7 @@ import { GlobalStyles } from "../constants/styles";
 import Button from "../components/UI/Button";
 import { ExpensesContext } from "../store/expenses-context";
 import ExpenseForm from "../components/ManageExpense/ExpenseForm";
+import { storeExpense } from "../util/htttp";
 
 function ManageExpenseScreen({ route, navigation }) {
     const expensesCtx = useContext(ExpensesContext);
@@ -36,6 +37,7 @@ function ManageExpenseScreen({ route, navigation }) {
         if (isEditing) {
             expensesCtx.updateExpense(editedExpenseId, expenseData);
         } else {
+            storeExpense(expenseData)
             expensesCtx.addExpense(expenseData);
         }
         navigation.goBack();
